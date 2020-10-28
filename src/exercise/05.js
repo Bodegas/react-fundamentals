f324059d5056565e84d57ad52bac526eefddfb4c
Bodegas <bodegas.mail@gmail.com>
2020-10-28 22:12:17 +0100

    Exercise 05

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,8 +1,8 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
-import React from 'react'
-import '../box-styles.css'
+import React from "react"
+import "../box-styles.css"
 
 // 💰 Use the className for the size and style (backgroundColor) for the color
 // 💰 each of the elements should also have the "box" className applied
@@ -12,9 +12,24 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = (
+  <div
+    className="box box--small"
+    style={{backgroundColor: "lightblue", fontStyle: "italic"}}
+  >
+    small lightblue box
+  </div>
+)
+const mediumBox = (
+  <div className="box box--medium" style={{backgroundColor: "pink", fontStyle: "italic"}}>
+    medium pink box
+  </div>
+)
+const largeBox = (
+  <div className="box box--large" style={{backgroundColor: "orange", fontStyle: "italic"}}>
+    large orange box
+  </div>
+)
 
 function App() {
   return (
@@ -27,3 +42,5 @@ function App() {
 }
 
 export default App
+
+
