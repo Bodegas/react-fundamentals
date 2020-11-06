460fd13d24820debd0a837492932f2cf29ff55d4
Bodegas <bodegas.mail@gmail.com>
2020-11-06 01:38:52 +0100

    Exercise 06 Extra 02

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,30 +1,40 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import React, {useRef} from "react"
+import React, {useRef, useState} from "react";
 
 function UsernameForm({onSubmitUsername}) {
   const inputRef = useRef(null);
+  const [error, setError] = useState(null);
 
   const handleSubmit = event => {
-    event.preventDefault()
-    onSubmitUsername(inputRef.current?.elements?.username?.value)
-  }
+    event.preventDefault();
+    onSubmitUsername(inputRef.current?.elements?.username?.value);
+  };
+
+  const handleChangeInput = event => {
+    const value = event?.target?.value;
+    const isValid = value === value.toLowerCase();
+    setError(isValid ? null : "Username must be lowercase");
+  };
 
   return (
     <form ref={inputRef} onSubmit={handleSubmit}>
       <div>
         <label htmlFor="username">Username:</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={handleChangeInput} />
+        <div style={{color: "red"}}>{error}</div>
       </div>
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={error}>
+        Submit
+      </button>
     </form>
-  )
+  );
 }
 
 function App() {
-  const onSubmitUsername = username => alert(`You entered: ${username}`)
-  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+  const onSubmitUsername = username => alert(`You entered: ${username}`);
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />;
 }
 
-export default App
+export default App;
